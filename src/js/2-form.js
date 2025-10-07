1ff81423490af28fffd2064fc48f0a1853dd186a
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-10-07 15:38:10 +0300

    task-2 done

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+const localFormData = localStorage.getItem('feedback-form-state');
+if (localFormData !== null) {
+  formData = JSON.parse(localFormData);
+  email.value = formData.email || '';
+  message.value = formData.message || '';
+}
+
+form.addEventListener('input', e => {
+  const { name, value } = e.target;
+  formData[name] = value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  formData = { name: '', message: '' };
+  form.reset();
+});
